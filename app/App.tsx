71ffeecdc4b3eb06a03a5e07066e21fb917d38eb
Jerica <jericahanna04@gmail.com>
2025-04-29 20:44:49 -0500

    Fixing small errors and also changing some of the styling component on the to do app. Color scheme white some background color and numbers

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,8 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
+Amplify.configure;
 
 export default function App() {
   return (
