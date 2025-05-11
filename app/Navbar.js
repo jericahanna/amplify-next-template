bcc0d567a64ffbe0e821017e49fa27280724557f
Jerica <jericahanna04@gmail.com>
2025-05-11 13:28:51 -0500

    editing the nav bar to make sure its perfect

--- app/Navbar.js
+++ app/Navbar.js
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-black text-white px-6 py-3 flex gap-4">
-      <Link href="/" className="hover:underline">Home</Link>
-      <Link href="/inspire" className="hover:underline">Inspire</Link>
-      <Link href="/moods" className="hover:underline">Moods</Link>
-      <Link href="/settings" className="hover:underline">About</Link>
+      <Link href="/">Home</Link>
+      <Link href="/inspire">Inspire</Link>
+      <Link href="/moods">Moods</Link>
+      <Link href="/about">About</Link>
     </nav>
   );
 }
