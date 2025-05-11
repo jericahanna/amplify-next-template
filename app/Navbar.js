0ffdaefb3706642473d25b7909f1a36b437743f3
Jerica <jericahanna04@gmail.com>
2025-05-11 13:27:26 -0500

    editing the nav bar

--- app/Navbar.js
+++ app/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <Link href="/" className="hover:underline">Home</Link>
       <Link href="/inspire" className="hover:underline">Inspire</Link>
       <Link href="/moods" className="hover:underline">Moods</Link>
-      <Link href="/settings" className="hover:underline">Settings</Link>
+      <Link href="/settings" className="hover:underline">About</Link>
     </nav>
   );
 }
